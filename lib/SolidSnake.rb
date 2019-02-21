f6a32c9987ccbb8f7a156bd79b904f16752bbf80
Ganesha Danu <ganeshadanu1@gmail.com>
2019-02-21 20:32:35 +0700

    generate recursive directory

--- lib/SolidSnake.rb
+++ lib/SolidSnake.rb
@@ -1,10 +1,18 @@
 require 'thor'
+require 'fileutils'
 
 module SolidSnake
     class CLI < Thor
         desc "generate TYPE NAME", "ex: generate Interactor Lorem"
         def generate type, name
-            filename = "#{name.capitalize}#{type.capitalize}.swift"
+            @name = name.capitalize
+            @type = type.capitalize
+            filename = "#{@name}#{@type}.swift"
+            dir = File.dirname "./components/#{@name}/#{@type}"
+            if File.directory? dir
+                FileUtils.mkdir_p "./components/#{@name}/#{@type}"
+            end
+
             puts "#{filename} file generated"
         end
     end
